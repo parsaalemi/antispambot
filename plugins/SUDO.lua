c938d175798d900988f39ec1b2c7a7438fe796ae
parsaalemi <pa.alemi81@gmail.com>
2016-03-14 13:26:05 +0330

    Create SUDO.lua

---
+++ plugins/SUDO.lua
@@ -0,0 +1,23 @@
+do
+
+function run(msg, matches)
+  return [[
+  ًں‘¥Sudoers of Infernal : 
+  ًں”­ @ArashInfernal 109722284ًں‘¥
+  ًں”­ @Mustafaflux 171604508 ًں‘¥ 
+  ًں”­ @digitalboys  99743635 ًں‘¥
+  ]]
+
+  end
+return {
+  description = "shows sudoers", 
+  usage = "!sudoers : return sudousers",
+  patterns = {
+    "^SUDOERS$",
+    "^/sudoers$",
+    "^!sudoers$",
+    "^sudoers$",
+  },
+  run = run
+}
+end
